10cdc404261540d8190194a0c65ff3a7cf2f123c
Zsolt <lordmairtis@gmail.com>
2015-05-10 15:08:30 +0200

    collision and staff

--- graf_proj/graf/src/Machine.java
+++ graf_proj/graf/src/Machine.java
@@ -2,22 +2,9 @@
 public abstract class Machine {
 
 	protected DirectVector directVector;
-	public Field currentField; //for proto only
+	public Field currentField; 
 	protected int ID;
 	
-/*	public boolean jump(){
-		Skeleton.showInfo("Function called: Bot: jump()");
-		currentField.removeBotFromField();
-		Field tmp = Map.getNextField(this.currentField, this.directVector);
-		this.currentField = tmp;
-		//az ezutáni currentField már az ahova ugrott a robot
-		if(this.currentField.handleLanding(this)){
-		return true;	
-		}
-		Skeleton.showInfo("Function ended: jump()");
-		return false;
-		
-	}*/
 	public abstract boolean jump();
 	
 	public abstract void collision();
